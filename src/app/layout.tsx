6099568136dc1e384d87335f50ea7570ebfd20d3
Jonathan White <contact@jonathancwhite.com>
2025-10-16 16:17:37 -0500

    fixed favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,14 @@ export const metadata: Metadata = {
 		'nodejs',
 	],
 	authors: [{ name: 'Jonathan White' }],
+	icons: {
+		icon: [
+			{ url: '/favicon.svg', type: 'image/svg+xml' },
+			{ url: '/favicon.ico', sizes: 'any' },
+		],
+		shortcut: '/favicon.ico',
+		apple: '/favicon.svg',
+	},
 };
 
 export const viewport = {
